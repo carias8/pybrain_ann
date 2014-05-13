c4a846c68264a40e0ef5532a1cf5e582d9be806d
carlos <carlos.arias@carepaths.com>
2014-05-12 22:00:22 -0400

    Made changes to reflect time series

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 from models.base_net import NeuralNet
-from models.trainer import Trainer
 
 
 
 # Teach network XOR function
-#pats = [
+#pat = [
 #    [[0,0], [0]],
 #    [[0,1], [1]],
 #    [[1,0], [1]],
 #    [[1,1], [0]]
 #]
 
-pats = [
-    [[1.0, 1.0], [1, 1]],
-    [[1.01, 0.99], [1, 1]],
+pat = [
+    [[0,0], [0]],
+    [[0,1], [1]],
+    [[1,0], [1]],
+    [[1,1], [0]]
 ]
 
 
-
 # create a network with two input, two hidden, and one output nodes
-n = NeuralNet(3, [4], 2)
+n = NeuralNet(2, [3, 3, 3], 2)
 #n.weights()
 # train it with some patterns
-trainer = Trainer()
-trainer.train(n, pats)
+n.train(5)
 # test it
-n.test(pats)
+n.test(5)
 #n.weights()
